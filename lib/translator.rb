16ba2214fc16b0c5bee61b44c528bca1e836b0be
jyang81 <joe.yang.design@gmail.com>
2019-03-01 23:58:17 -0800

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,42 @@
+require 'pry'
+require 'yaml'
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(library)
+    hash = {}
+    library = YAML.load_file('./lib/emoticons.yml')
+    hash['get_meaning'] = {}
+    hash['get_emoticon'] = {}
+    library.each do |meaning,i|
+            hash.values[0][i[1]] = meaning
+            hash.values[1][i[0]] = i[1]
+    end
+#    binding.pry
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(library, emoticon)
+    library = YAML.load_file('./lib/emoticons.yml')
+    new = load_library('./lib/emoticons.yml')
+#    binding.pry
+    answer = ""
+    if new["get_emoticon"].keys.include?(emoticon)
+        answer = new["get_emoticon"][emoticon]
+    else
+        answer = "Sorry, that emoticon was not found"
+    end
+    answer
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(library, emoticon)
+    library = YAML.load_file('./lib/emoticons.yml')
+    new = load_library('./lib/emoticons.yml')
+    answer = ""
+    if new["get_meaning"].keys.include?(emoticon)
+        answer = new["get_meaning"][emoticon]
+        else
+        answer = "Sorry, that emoticon was not found"
+    end
+#    binding.pry
+    answer
 end
